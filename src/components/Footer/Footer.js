1659cfb377f7d1e4bca69221368e6084387ead87
mcfrems <matthew.fremstad@gmail.com>
2023-02-15 20:54:34 -0600

    footer css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import "./Footer.css"
+import { AiOutlineCopyrightCircle } from "react-icons/ai";
+
 
 function Footer() {
   return (
     <div className="footer-container">
-        Footer
+      <div className="footer-left-container">
+        <a href="">About Us</a>
+        <a href="">Terms & Conditions</a>
+        <a href="">Privacy & Cookie Policies</a>
+      </div> 
+      <div className="footer-right-container">
+        <p>2022</p>
+        <div className="footer-right-copyright">
+          <AiOutlineCopyrightCircle />
+          <p>UniLife</p>
+        </div>
+      </div> 
     </div>
   )
 }
